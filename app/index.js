a505f93674ac6b70fd6a8171b56aa97031bd08d3
Andreas <andrzej.ratajski@outlook.com>
2021-12-17 18:27:31 +0100

    Simple rules engine for fileds validation

--- app/index.js
+++ app/index.js
@@ -1,49 +1,23 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-const { Engine } = require('json-rules-engine')
-
+const { Engine } = require('json-rules-engine');
+const flamcoRule = require("./rules/flamco-metadata-rule");
+const userAuthRule = require("./rules/user-authorized-rule");
+ 
 /////////////////////////////////////////////////////////////
 // FILES & RULES
 /////////////////////////////////////////////////////////////
-const PATH = './dev-data/request-body.json'
+const PATH = 'app/dev-data/request-body.json'
+const RULE_PATH = 'app/rules/flamco-metadata-rule.json'
+
+console.log(`Read rule: ${flamcoRule["event"]["type"]}`)
+console.log(`Read rule: ${userAuthRule["event"]["type"]}`)
 
 // Invoke Rules Engine 
 let engine = new Engine()
-
-// Define Rules (can - or even should - be an external JSON file)
-engine.addRule({
-  conditions: {
-    any: [{
-      all: [{
-        fact: 'B2',
-        operator: 'lessThanInclusive',
-        value: 20
-      }, {
-        fact: 'B3',
-        operator: 'greaterThanInclusive',
-        value: 100
-      }]
-    }, {
-      all: [{
-        fact: 'B2',
-        operator: 'greaterThanInclusive',
-        value: 100
-      }, {
-        fact: 'B3',
-        operator: 'lessThanInclusive',
-        value: 20
-      }]
-    }]
-  },
-  event: {
-    type: 'OverwriteB4Filed',
-    params: {
-      message: 'Change value of the field B4',
-      value: '0'
-    }
-  }
-})
+engine.addRule(flamcoRule);
+engine.addRule(userAuthRule);
 
 /////////////////////////////////////////////////////////////
 // SERVER
@@ -51,64 +25,69 @@ engine.addRule({
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
     let data = '';
-    let fetched;
     
-
     req.on('data', chunk => {
       data += chunk;
     })
     req.on('end', () => {
-      // Queue asynchronious calls so they will be executed in an order
-      fs.writeFile(PATH, `${data}`, 'utf-8', err => {
-        console.log(`Request body has been written into a ${PATH}`);
-        fs.readFile(PATH, 'utf-8', (err2, fetched) => {
-
-          // Overview page
-          if (pathname === '/') {
-            res.writeHead(200, {
-              'Content-type': 'text/html'
-            });
-          res.end('<h1>Hello. Please use API at: /api</h1>');
-
-
-          // API
-          } else if (pathname === '/api') {
-            console.log('Rules Engine API called!');
-            res.writeHead(200, {
-              'Content-type': 'application/json'
-            });
-
-            // Get some fields that will be used as facts for RE
-            let modified = JSON.parse(fetched);
-            let filedB2 = modified['params'][0];
-            let filedB3 = modified['params'][1];
-
-            let facts = {
-              B2: filedB2['value'],
-              B3: filedB3['value']
-            };
-
-            // Evaluate the Rules Engine with obtained facts and manipulate output
-            engine
-            .run(facts)
-            .then(({ events }) => {
-              events.map(event => {
-                modified['params'][2]['value'] = event.params.value;
-                console.log(`Rules Engine Message: ${event.params.message} to the value of ${event.params.value}`);
-                res.end(JSON.stringify(modified));
-              })
-            });
-
-
-          // Not found
-          } else {
-            res.writeHead(404, {
-              'Content-type': 'text/html',
-            });
-          res.end('<h1>Page not found!</h1>');
-          }
+      // Overview page
+      if (pathname === '/') {
+        res.writeHead(200, {
+          'Content-type': 'text/html'
         });
-      });
+      res.end('<h1>Hello. Please use API at: /api</h1>');
+      }
+
+      // API 
+      else if (pathname === '/api') {
+        console.log('Rules Engine API called!');
+        // fs.writeFile(PATH, data, 'utf-8', err => {
+        //   console.log(`Request body has been written into a ${PATH}`);
+        // });
+
+        // Get some fields that will be used as facts for RE
+        const request_facts = JSON.parse(data);
+        const userGroup = request_facts['userGroup'];
+        const isUserAuthorized = request_facts['isUserAuthorized'];
+
+        const facts = {
+          userGroup: userGroup,
+          userAuthorized: isUserAuthorized
+        };
+
+        res.writeHead(200, {
+          'Content-type': 'application/json'
+        });
+
+      // Evaluate the Rules Engine with obtained facts and manipulate output
+      engine
+      .run(facts)
+      .then((events) => {
+        if (events["almanac"]["events"]["failure"].length > 0) {
+          console.log(`There's been failures when evaluating rules....`);
+          events["almanac"]["events"]["failure"].map( (failure) => (
+            console.log(`----> Type: ${failure["type"]}, Message: ${failure["params"]["message"]}`)
+          ));
+          res.end(JSON.stringify( {errorMessage: "Failure during facts validation"} ));
+        }
+        else {
+          console.log(events)
+          const userFiledsRule = events["almanac"]["events"]["success"]
+            .filter( (success) => (
+              success["type"] === flamcoRule["event"]["type"]
+          ));
+          res.end(JSON.stringify(userFiledsRule[0]["params"]["value"]));
+        }
+        })
+    }
+
+      // Not found
+      else {
+        res.writeHead(404, {
+          'Content-type': 'text/html',
+        });
+        res.end('<h1>Page not found!</h1>');
+      }
     });
 });
 
